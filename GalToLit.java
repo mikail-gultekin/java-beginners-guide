44220186dc149edd348f5870b562ee7c88a00415
mikail-gultekin <gultekinmikail@gmail.com>
2020-03-07 20:08:57 +0100

    gallons to liters convert sample

---
+++ GalToLit.java
@@ -0,0 +1,14 @@
+// this program converts gallons to liters
+class GalToLit
+{
+	public static void main(String args[])
+	{
+		double gallons;
+		double liters;
+		
+		gallons = 10;
+		liters = gallons * 3.7854;
+		
+		System.out.println(gallons + " gallons is " + liters + " liters");
+	}
+}
